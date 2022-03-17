f66ce579c9b965ed277be2cb3504506cce9fc6bb
Dimitry Wu <grayfen@gmail.com>
2022-03-17 21:47:15 +0800

    wee3 assignment revised

--- static/script/index.js
+++ static/script/index.js
@@ -1,7 +1,7 @@
 let page = 0;
 let keyword = "";
-let src = "http://127.0.0.1:3000/api/attractions?page=" + page + "&keyword=" + keyword;
-// let src = "http://3.230.236.135:3000/api/attractions?page=" + page + "&keyword=" + keyword;
+// let src = "http://127.0.0.1:3000/api/attractions?page=" + page + "&keyword=" + keyword;
+let src = "http://3.230.236.135:3000/api/attractions?page=" + page + "&keyword=" + keyword;
 
 
 // 建立照片資料元素到網頁
